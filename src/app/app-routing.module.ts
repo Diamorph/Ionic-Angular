67493a47b83d42bf17e07b597084ba7bea40ea42
diamorph <diamorph1309@gmail.com>
2023-08-17 20:59:13 -0400

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppRouting } from './core/routing/app.routing';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  {path: '', redirectTo: AppRouting.Recipies, pathMatch: 'full'},
+  {path: AppRouting.Recipies, children: [
+    {path: '', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule)},
+    {path: `:${AppRouting.RecipeId}`, loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then(m => m.RecipeDetailPageModule)}
+  ] }
 ];
 
 @NgModule({
